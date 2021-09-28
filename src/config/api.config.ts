d347dc7fbbc1e9cb011f53d2dceaba8e8a253b49
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-09-28 09:14:20 -0300

    Testando app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://spring-ionic-noriyuki.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-noriyuki.s3.sa-east-1.amazonaws.com"
 }
 
